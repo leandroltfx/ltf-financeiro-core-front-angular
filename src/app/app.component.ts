846b7bf7ee34af8de50f411c94e9d1ebd5f7c287
leandrobr95 <leandrobsb55@gmail.com>
2023-03-01 23:11:44 -0300

    removendo variável não utilizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ltf-financeiro-core-front-angular';
-}
+export class AppComponent { }
