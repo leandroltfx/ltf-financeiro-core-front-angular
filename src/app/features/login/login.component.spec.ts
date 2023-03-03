55f093c4a4a88b30e57926d3a7cfd37263e16902
leandrobr95 <leandrobsb55@gmail.com>
2023-03-03 16:53:11 -0300

    adicionando link para cadastro de usuário

--- src/app/features/login/login.component.spec.ts
+++ src/app/features/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzFormModule } from 'ng-zorro-antd/form';
@@ -22,6 +23,7 @@ describe('LoginComponent', () => {
       imports: [
         ReactiveFormsModule,
         HttpClientTestingModule,
+        RouterTestingModule,
 
         NzCardModule,
         NzFormModule,
