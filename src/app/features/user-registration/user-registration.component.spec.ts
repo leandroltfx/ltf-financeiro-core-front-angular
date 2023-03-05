a4db5df0f5073ee417dd8ec28600275149f382e1
leandrobr95 <leandrobsb55@gmail.com>
2023-03-05 09:51:12 -0300

    removendo import não utilizado

--- src/app/features/user-registration/user-registration.component.spec.ts
+++ src/app/features/user-registration/user-registration.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
