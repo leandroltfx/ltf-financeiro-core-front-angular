6e3a2430594b18d262969499990242c07df443b2
leandrobr95 <leandrobsb55@gmail.com>
2023-03-08 11:30:11 -0300

    criando componentes header e menu para a home

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
+
+    NzMenuModule,
+    NzIconModule,
+    NzButtonModule,
+    NzDropDownModule,
+
     HomeRoutingModule
   ]
 })
